08bcfc177af589f82c27cabc981a2916e4a38128
Drew Keat <thedrewkeat@gmail.com>
2021-09-21 22:21:33 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()*10) + 1
+
+let num3 = 10
+let num4 = 6
+let mod = num3 % num4
+
+let arr = [1, 5, 6, 2, 20, 4, 9]
+let max = Math.max(...arr)
+
+console.log(max)
